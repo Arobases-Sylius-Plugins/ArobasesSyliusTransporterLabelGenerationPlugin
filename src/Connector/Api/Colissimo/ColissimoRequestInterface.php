e751dfd6204841a662b4a41f990995392b99f00f
Arobases-e-commerce <119339399+Arobases-e-commerce@users.noreply.github.com>
2023-11-29 12:13:44 +0100

    Update and rename LabelGeneratorInterface to ColissimoRequestInterface.php

--- src/Connector/Api/Colissimo/ColissimoRequestInterface.php
+++ src/Connector/Api/Colissimo/ColissimoRequestInterface.php
@@ -5,8 +5,9 @@ namespace Arobases\SyliusTransporterLabelGenerationPlugin\Connector\Api\Colissim
 use Sylius\Component\Core\Model\Channel;
 use Arobases\SyliusTransporterLabelGenerationPlugin\Entity\Label;
 use Arobases\SyliusTransporterLabelGenerationPlugin\Entity\Transporter;
+use App\Entity\Order\Order;
 
-interface LabelGeneratorInterface
+interface ColissimoRequestInterface
 {
     public function generateLabel(Channel $channel, Label $label, Transporter $transporter, string $outputPrintingType, string $depositDate): array;
 
